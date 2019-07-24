3a210e29305dd62e7889080e2f57a1a6d16ac310
petro.byndas <petro.byndas@711media.de>
2019-07-24 14:13:30 +0300

    Rewrite moving element with transition

--- module/zoom.js
+++ module/zoom.js
@@ -3,19 +3,23 @@ export default class Zoom {
     selector = '.zoom-image',
     zoomInBtn = null,
     zoomOutBtn = null,
-    zoomStep = 0.12,
-    zoomCurrent = 1,
+    scaleStep = 0.12,
+    targetScale = 1,
     zoomMin = 0.1,
   }) {
     this.selector = typeof selector === 'string' ? document.querySelector(selector) : selector;
     this.image = this.selector.querySelector('img');
     this.zoomInBtn = typeof zoomInBtn === 'string' ? document.querySelector(zoomInBtn) : zoomInBtn;
     this.zoomOutBtn = typeof zoomOutBtn === 'string' ? document.querySelector(zoomOutBtn) : zoomOutBtn;
-    this.zoomStep = zoomStep;
-    this.zoomCurrent = zoomCurrent;
+    this.scaleStep = scaleStep;
+    this.targetScale = targetScale;
+    this.targetOffsetX = 0;
+    this.targetOffsetY = 0;
     this.zoomMin = zoomMin;
-    this.imageW = this.image.offsetWidth;
-    this.imageH = this.image.offsetHeight;
+    this.imageW = this.image.clientWidth;
+    this.imageH = this.image.clientHeight;
+    this.selectorW = this.selector.clientWidth;
+    this.selectorH = this.selector.clientHeight;
 
     this.init();
   }
@@ -23,9 +27,16 @@ export default class Zoom {
   init() {
     if (this.selector === null) return;
     this.createBtn();
+    this.centerElement();
     this.image.addEventListener('mousedown', ev => this.handleMouseDown(ev));
     this.image.addEventListener('dblclick', () => this.zoomInOut(true));
     this.image.ondragstart = () => false;
+  }
+
+  centerElement() {
+    this.targetOffsetX = (this.selectorW - this.imageW) / 2;
+    this.targetOffsetY = (this.selectorH - this.imageH) / 2;
+    this.moveElementTo();
   }
 
   createBtn() {
@@ -47,14 +58,11 @@ export default class Zoom {
 
   zoomInOut(condition) {
     if (condition) {
-      this.zoomCurrent += this.zoomStep;
-    } else if (this.zoomCurrent - this.zoomStep > this.zoomMin) {
-      this.zoomCurrent -= this.zoomStep;
+      this.targetScale += this.scaleStep;
+    } else if (this.targetScale - this.scaleStep > this.zoomMin) {
+      this.targetScale -= this.scaleStep;
     }
-    this.image.style.width = `${this.imageW * this.zoomCurrent}px`;
-    this.image.style.height = `${this.imageH * this.zoomCurrent}px`;
-    this.image.style.maxWidth = 'none';
-    this.image.style.maxHeight = 'none';
+    this.moveElementTo();
   }
 
   handleMouseDown(event) {
@@ -78,14 +86,21 @@ export default class Zoom {
       imageX = pageX - left - offsetX;
       imageY = pageY - top - pageYOffset - offsetY;
 
+      // Fix pointer position on image if scaled
+      if (this.targetScale !== 1) {
+        imageX -= (offsetX * this.targetScale - ((this.imageW * this.targetScale) / 2)) - (offsetX - (this.imageW / 2));
+        imageY -= (offsetY * this.targetScale - ((this.imageH * this.targetScale) / 2)) - (offsetY - (this.imageH / 2));
+      }
+
       // Prevent wrong position of image
       this.image.style.right = 'auto';
       this.image.style.bottom = 'auto';
 
       // Check border position of image wrapper
       if (pageX >= left && pageX <= right && pageY - pageYOffset >= top && pageY - pageYOffset <= bottom) {
-        this.image.style.top = `${imageY}px`;
-        this.image.style.left = `${imageX}px`;
+        this.targetOffsetX = imageX;
+        this.targetOffsetY = imageY;
+        this.moveElementTo();
       } else {
         document.removeEventListener('mousemove', imageMove);
       }
@@ -99,5 +114,23 @@ export default class Zoom {
       document.removeEventListener('mousemove', imageMove);
       this.image.onmouseup = null;
     });
+  }
+
+  moveElementTo() {
+    this.image.style.cssText = `-moz-transform : 
+    translate( ${this.targetOffsetX}px, ${this.targetOffsetY}px) 
+    scale(${this.targetScale}); 
+    -ms-transform : 
+    translate(${this.targetOffsetX}px, ${this.targetOffsetY}px) 
+    scale(${this.targetScale}); 
+    -o-transform : 
+    translate(${this.targetOffsetX}px, ${this.targetOffsetY}px) 
+    scale(${this.targetScale}); 
+    -webkit-transform : 
+    translate(${this.targetOffsetX}px, ${this.targetOffsetY}px) 
+    scale(${this.targetScale}); 
+    transform : 
+    translate3d(${this.targetOffsetX}px, ${this.targetOffsetY}px, 0) 
+    scale3d(${this.targetScale}, ${this.targetScale}, 1);`;
   }
 }
