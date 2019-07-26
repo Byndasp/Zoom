92b754689512ed64167318b6b371b04e9950e219
petro.byndas <petro.byndas@711media.de>
2019-07-26 09:35:30 +0300

    Revert

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import Zoom from "../zoom/zoom.js";
+import Zoom from "./zoom/zoom.js";
 
 const zoomedImages = document.querySelectorAll('.zoom-img_wrap');
 for (let i = 0; i < zoomedImages.length; i += 1) {
